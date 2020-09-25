84df4f9e62f5b5a91e8e01175be115f9fa43ecb4
Michael Elwood <melwood@proformex.com>
2020-09-24 20:33:15 -0400

    first non repeating char return codesignal

---
+++ FirstNonRepeatingChar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+char firstNotRepeatingCharacter(string s)
+{
+    Dictionary<char, int> dup = new Dictionary<char, int>();
+
+    foreach (char c in s.ToCharArray())
+    {
+        if (dup.Keys.Contains(c))
+        {
+            dup[c] += 1;
+        }
+        else
+            dup.Add(c, 1);
+
+    }
+    if (dup.Values.Contains(1))
+        return dup.First(b => b.Value == 1).Key;
+    else
+        return '_';
+
+}
