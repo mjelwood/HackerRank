7b49e5f354eebda4dc0dc2b84390b3808ab28a4f
Michael Elwood <melwood@proformex.com>
2020-09-24 20:11:17 -0400

    finish loop and add values

--- Staircase/solution.cs
+++ Staircase/solution.cs
@@ -12,15 +12,29 @@ using System.Text.RegularExpressions;
 using System.Text;
 using System;
 
-class Solution {
+class Solution
+{
+
+    //Notes:
+    // Best to start loop at 1
+    // To right align one way is create string of empty space based on passed in value
+    // C# has String which an override allows for duplicating a character n times
 
     // Complete the staircase function below.
-    static void staircase(int n) {
+    static void staircase(int n)
+    {
 
+        for (int i = 1; i <= n; i++)
+        {
+            String space = new String(' ', n - i);
+            String s = new String('#', i);
 
+            Console.WriteLine(space + s);
+        }
     }
 
-    static void Main(string[] args) {
+    static void Main(string[] args)
+    {
         int n = Convert.ToInt32(Console.ReadLine());
 
         staircase(n);
